8fa9aed0e2f417b2a6fd6b614ec74ebc5f085924
danlowes1 <dan141@gmail.com>
2025-08-15 15:37:00 +0100

    reder issue fix 7

--- danlowes-portfolio/src/components/Contact.jsx
+++ danlowes-portfolio/src/components/Contact.jsx
@@ -22,7 +22,7 @@ export const Contact = () => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
-      .catch(() => alert("Oops! Something went wrong. Please try again."));
+      .catch(() => alert("Oopsa daisy! Something went wrong. Please try again."));
   };
 
   return (
@@ -32,7 +32,6 @@ export const Contact = () => {
     >
         <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
             Get In Touch
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
